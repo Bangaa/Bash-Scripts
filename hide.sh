066e60330a9e451d7633ce23d6efbed9a0e94134
Ian Mejias <ian.mejias@usach.cl>
2014-05-24 20:58:33 -0400

    Se modifica el algoritmo para que oculte archivos donde sea.

--- hide.sh
+++ hide.sh
@@ -6,13 +6,36 @@
 #
 #  - Desocultar (mostrar)
 
+get_file_begin ()
+{
+    if [ $# -lt 1 ];then
+	exit 1
+    fi
+
+    (( INDEX = 0 ))
+    for (( i=${#1} ; i>=1; i-- ));do
+	if [ "${1:i-1:1}" == "/" ];then
+	    (( INDEX = $i ))
+	    break
+	fi
+    done
+
+    echo $INDEX
+}
+
 if [ $# -lt 1 ];then
     echo "usage: hide <file> ..." 
     return
 fi
 
 for file in "$@";do
-    if [ ${file:0:1} != "." ];then
-	mv "$file" ".$file"
+    (( SPLIT_AT= $(get_file_begin $file) ))
+    DIR=${file:0:SPLIT_AT}
+    FILENAME=${file:SPLIT_AT}
+
+    if ! [ -e $file ];then
+	echo "error: No existe el fichero o directorio \"$file\" " >&2
+    elif [ ${FILENAME:0:1} != "." ];then
+	mv "$DIR""$FILENAME" "$DIR"."$FILENAME"
     fi
 done 
